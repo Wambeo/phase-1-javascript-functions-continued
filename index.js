850382eb50908ef4b69fcb6fb1ab664d7e719a65
Samuel Wambeo <samuelwambeo9@gmail.com>
2024-03-19 15:27:20 +0300

    scope complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(target= "roller-skate"){
+    return `This Saturday, I want to ${target}!`;
+}
+
+function mondayWork(uses = "go to the office"){
+    return `This Monday, I will ${uses}.`
+}
+
+function wrapAdjective(result){
+   
+    return function(par = "special"){
+       return `You are ${result}${par}${result}!`
+   };
+   
+}
